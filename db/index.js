eef8c225eab78228fffb1acaff16fa0016017d6d
Utkarsh <utkarshmishra2001@gmail.com>
2022-04-28 13:25:54 +0530

    Working NodeMailer Code

--- db/index.js
+++ db/index.js
@@ -1,30 +1,37 @@
+var nodemailer = require('nodemailer')
 
-  
-    var nodemailer = require('nodemailer')
-    
-    var transport = nodemailer.createTransport({
-        service:'gmail',
-        auth:{
-            user:'bansalnishant316@gmail.com',
-            pass:'nishant16'
-        }
-    })
-    
-    //Send mail
-    
-    var mailOptions = {
-        from:"utkarshmishra2001@gmail.com",
-        to:"bansalnishant1653@gmail.com",
-        subject:"Email NodeJS test",
-        text:"Hello Mr. B Nishanth Bansa, tu mujhe janta hai tereko pata nahi lekin tu janta hai"
-    };
-    
-    transport.sendMail(mailOptions,function(error, info) {
-        if(error){
-            console.log(error);
-        }
-        else
-        {
-            console.log("Email Sent" + info.response);
-        }
-    })
+export function emailSender(email)
+{
+
+var transport = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 587,
+    ignoreTLS: false,
+    secure: false,
+    service:'gmail',
+    auth:{
+        user:'bansalnishant3516@gmail.com',
+        pass:'nishant16'
+    }
+})
+
+//Send mail
+
+var mailOptions = {
+    from:"utkarshmishra2001@gmail.com",
+    to:email,
+    subject:"Email NodeJS test",
+    text:"Hello Mr. B Nishanth Bansal This is sent using dynamic text feild"
+};
+
+transport.sendMail(mailOptions,function(error, info) {
+    if(error){
+        console.log(error);
+    }
+    else
+    {
+        console.log("Email Sent" + info.response);
+    }
+})
+
+} 
